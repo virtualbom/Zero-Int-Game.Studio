fde8e3d96c6997d0520bbae36360295f0dfc218e
virtualbom <99601892+virtualbom@users.noreply.github.com>
2026-01-30 22:13:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("active");
+}
+
+function copyDiscord() {
+  navigator.clipboard.writeText("virtualhino");
+  alert("Copied Discord username: virtualhino");
+}
